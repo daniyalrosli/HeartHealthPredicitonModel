c428cf9eed7b885c7efc15893c35895815cdc91e
Suvan Dommeti <suvandommeti0@gmail.com>
2023-04-12 00:02:03 -0400

    Update Heart Health Model to work

--- pages/1_Heart Health Calculator.py
+++ pages/1_Heart Health Calculator.py
@@ -1,255 +1,198 @@
-import streamlit as st
-# For ML models
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
-import imblearn.over_sampling
+from scipy import stats
+import streamlit as st
+import sklearn as sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 
-# Filter warnings
-warnings.filterwarnings('ignore')
+st.header("Asian Heart Disease Risk Calculator")
 
-# Add a header to the app
-st.title("Asian Heart Disease Risk Calculator")
-st.caption("Please fill out the questionnaire below to see if you are at risk of diabetes.")
-# Load the data
-df = pd.read_csv('data/heart_2020_cleaned.csv')
-data = df
 
-st.markdown("""---""") 
+##Loading Data
+df = pd.read_csv('heart_2020_cleaned.csv')
+newdf = df
 
-# Define a function to get user input
+st.write("Our data is below:")
+st.write(newdf.head(20))
+#st.write("We will now filter this data to focus on South Asian subjects for our model.")
+#train = train[train["Race"] == "Asian"]
+#st.write(train)
+
 def user_input_features():
-
-  BMI_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Enter BMI:</p>'
-  st.markdown(BMI_q, unsafe_allow_html=True)
-  st.caption("Body mass index (BMI) is calculated by dividing an individual's weight in kilograms by their height in meters squared. For example, if an individual weighs 70 kilograms and is 1.75 meters tall, their BMI would be calculated as follows:")
-  st.caption("BMI = 70 / (1.75 x 1.75) = 22.9")
-  BMI = st.slider('', 0.0, 110.0, 55.0, key = "l")
-  st.caption("You selected this option: " + str(BMI))
-
-  st.text("\n")
-  age_q = '<p style="font-family:Sans-Serif; font-size: 20px;">How old are you?</p>'
-
-  st.markdown(age_q, unsafe_allow_html=True)
-  st.caption('Slider is in years')
-  age = st.slider('', 0, 100, 50)
-  st.caption("You selected this option: " + str(age))
-
-  factor_age = 0
-  if age in range(0, 30, 1):
-    factor_age = 0
-  elif age in range(30,45,1):
-    factor_age = 1
-  elif age in range(45, 60, 1):
-    factor_age = 2
-  elif age in range(60, 70, 1):
-    factor_age = 3
-  elif age in range(70,75,1):
-    factor_age = 4
-  elif age in range(75, 80,1):
-    factor_age = 5
-  elif age in range(80, 101, 1):
-    factor_age = 6
-  st.text("\n")
-
-  sex_q = '<p style="font-family:Sans-Serif; font-size: 20px;">What is your sex?</p>'
-  st.markdown(sex_q, unsafe_allow_html=True)
-  sex = st.selectbox("(Male or Female", ["Male", "Female"], key = "e")
-  sex_str = 'You selected this option: ' + str(sex)
-  st.caption(sex_str)
-
-  bin_sex = 0
-  if sex == "Male":
-    bin_sex= 1
-
-  st.text("\n")
-  smoke_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Are you a heavy smoker?</p>'
-  st.markdown(smoke_q, unsafe_allow_html=True)
-  st.caption("A heavy smoker is someone who has smoked over 100 cigarettes in their lifetime.")
-  smoke = st.selectbox("Yes or No", ["No", "Yes"], key = "a")
-  st.caption("You selected this option: " + str(smoke))
-
-  bin_smoke = 0
-  if smoke == "Yes":
-    bin_smoke = 1
-    
-  st.text("\n")
-  alc_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Are you a heavy drinker?</p>'
-  st.markdown(alc_q, unsafe_allow_html=True)
   
-  st.caption("A heavy drinker is someone how drinks greater than 14 drinks per week as a man, and greater than 7 drinks per week as a woman.") 
-  alc = st.selectbox("Yes or No", ["No", "Yes"], key = "b")
-  st.caption("You selected this option: " + str(alc))
+  st.write("**Please fill out the questionnaire below to see if you are at risk of diabetes:**")
 
-  bin_alc = 0
-  if alc == "Yes":
-    bin_alc = 1
+  st.write("""**1. Enter BMI:**""") 
+  BMI = st.slider('', 0.0, 110.0, 55.0)
+  st.write("""**You selected this option:**""",BMI)
 
+  st.write("""**2. Have you smoked over 100 cigarettes in your lifetime?**""") 
+  smoke = st.selectbox("(Yes or No", ["Yes", "No"], key = "a")
+  st.write("""**You selected this option:**""",smoke)
 
-  st.text("\n")
-  phys_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Of the last 30 days, roughly how many have you experienced physical pain or illness?</p>'
-  st.markdown(phys_q, unsafe_allow_html=True)
+  st.write("""**3. Are you a heavy drinker? (>14 drinks per week for men, >7 drinks per week for women)**""") 
+  alc = st.selectbox("(Yes or No", ["Yes", "No"], key = "b")
+  st.write("""**You selected this option:**""",alc)
+
+  st.write("""**4. Have you ever had a stroke?**""") 
+  stroke = st.selectbox("(Yes or No", ["Yes", "No"], key = "c")
+  st.write("""**You selected this option:**""", stroke)
+  
+  st.write("""**5. Of the last 30 days, how many would you consider \'bad\' days physically?**""") 
   physical = st.slider('', 0, 30, 15, key = "1")
-  st.caption("You selected this option: " + str(physical))
+  st.write("""**You selected this option:**""", physical)
 
-  st.text("\n")
-  men_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Of the last 30 days, roughly how many would you consider \'bad\' days mentally?</p>'
-  st.markdown(men_q, unsafe_allow_html=True)
+  st.write("""**6. Of the last 30 days, how many would you consider \'bad\' days mentally?**""") 
   mental = st.slider('', 0, 30, 15, key = "2")
-  st.caption("You selected this option: " + str(mental))
+  st.write("""**You selected this option:**""", mental)
 
-  st.text("\n")
-  ex_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Have you exercised in the past 30 days?</p>'
-  st.markdown(ex_q, unsafe_allow_html=True)
-  exercise = st.selectbox("(Yes or No)", ["No", "Yes"], key = "g")
-  st.caption("You selected this option: " + str(exercise))  
+  st.write("""**7. Do you have difficulty climbing up stairs?**""") 
+  climb = st.selectbox("(Yes or No", ["Yes", "No"], key = "d")
+  st.write("""**You selected this option:**""", climb)
 
-  bin_exercise = 0
-  if exercise == "Yes":
-    bin_exercise = 1
+  st.write("""**8. What is your sex?**""") 
+  sex = st.selectbox("(Male or Female", ["Male", "Female"], key = "e")
+  st.write("""**You selected this option:**""", sex)
 
-  st.text("\n")
-  sleep_q = '<p style="font-family:Sans-Serif; font-size: 20px;">How long do you sleep in a day on average?</p>'
-  st.markdown(sleep_q, unsafe_allow_html=True)
-  st.caption('Slider is in hours')
+  st.write("""**9. Enter your age:**""") 
+  age = st.slider('', 0, 100, 50)
+  st.write("""**You selected this option:**""", age)
+
+  age_cat= ""
+
+  if age <= 29:
+    age_cat = "25-29"
+  elif age <= 34:
+    age_cat = "30-34"
+  elif age <= 39:
+    age_cat = "35-39"
+  elif age <= 44:
+    age_cat = "40-44"
+  elif age <= 49:
+    age_cat = "45-49"
+  elif age <= 54:
+    age_cat = "50-54" 
+  elif age <= 59:
+    age_cat = "55-59"
+  elif age <= 64:
+    age_cat = "60-64"  
+  elif age <= 69:
+    age_cat = "65-69"  
+  elif age <= 74:
+    age_cat = "70-74" 
+  elif age <= 79:
+    age_cat = "75-79"  
+  else:
+    age_cat = "older than 80"
+  
+  st.write("""**10. Have you ever been told you are diabetic?**""") 
+  diabetes = st.selectbox("(Yes or No)", ["Yes", "No"], key = "f")
+  st.write("""**You selected this option:**""", diabetes)  
+  
+  st.write("""**11. Have you exercised in the past 30 days?**""") 
+  exercise = st.selectbox("(Yes or No)", ["Yes", "No"], key = "g")
+  st.write("""**You selected this option:**""", exercise)  
+
+  st.write("""**12. How much do you sleep in a day (on avg):**""") 
   sleep = st.slider('', 0, 24, 12)
-  st.caption("You selected this option: " + str(sleep)) 
-  st.text("\n")
-
-  stroke_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Have you ever had a stroke?</p>'
-  st.markdown(stroke_q, unsafe_allow_html=True)
-  stroke = st.selectbox("(Yes or No", ["No", "Yes"], key = "c")
-  st.caption("You selected this option: " + str(stroke))
+  st.write("""**You selected this option:**""", sleep) 
   
-  bin_stroke = 0
-  if stroke == "Yes":
-    bin_stroke = 1
+  st.write("""**13. How would you consider your general health?**""") 
+  gen_health = st.selectbox("(Poor, Fair, Good, Very Good, Excellent)", ["Poor", "Fair", "Good", "Very Good", "Excellent"])
+  st.write("""**You selected this option:**""", gen_health)
+
+  st.write("""**14. Have you ever been told you have asthma?**""") 
+  asthma = st.selectbox("(Yes or No)", ["Yes", "No"], key = "h")
+  st.write("""**You selected this option:**""", asthma)  
+
+  st.write("""**15. Have you ever been told you have kidney disease?**""") 
+  kidney = st.selectbox("(Yes or No)", ["Yes", "No"], key = "i")
+  st.write("""**You selected this option:**""", kidney)
   
+  st.write("""**16. Have you ever been told you have skin cancer?**""") 
+  cancer = st.selectbox("(Yes or No)", ["Yes", "No"], key = "j")
+  st.write("""**You selected this option:**""", cancer)
 
-  st.text("\n")
-  db_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Have you ever been told you\'re diabetic?</p>'
-  st.markdown(db_q, unsafe_allow_html=True)
-  diabetes = st.selectbox("(Yes or No)", ["No", "Yes"], key = "f")
-  st.caption("You selected this option: " + str(diabetes))  
-  
-  bin_diabetes = 0
-  if diabetes == "Yes":
-    bin_diabetes = 1
-
-
-
-  st.text("\n")
-  as_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Have you ever been told you have asthma?</p>'
-  st.markdown(as_q, unsafe_allow_html=True)
-  asthma = st.selectbox("(Yes or No)", ["No", "Yes"], key = "h")
-  st.caption("You selected this option: " + str(asthma))  
-
-  bin_asthma = 0
-  if asthma == "Yes":
-    bin_asthma = 1
-  st.text("\n")
-  kd_q = '<p style="font-family:Sans-Serif; font-size: 20px;">Have you ever been told you have kidney disease?</p>'
-  st.markdown(kd_q, unsafe_allow_html=True)
-  kidney = st.selectbox("(Yes or No)", ["No", "Yes"], key = "i")
-  st.caption("You selected this option: " + str(kidney))
-
-  bin_kidney = 0
-  if kidney == "Yes":
-    bin_kidney = 1
-  
-  st.text("\n")
-  data_user = {'BMI': BMI, 'Smoking': smoke, 'AlcoholDrinking    ': alc, 'Stroke': stroke, 'PhysicalHealth': physical, 'MentalHealth': mental, 'Sex': sex,  'Diabetic': diabetes, 'PhysicalActivity    ': exercise, 'SleepTime': sleep,'AgeCategory': age, 'Asthma': asthma, 'KidneyDisease': kidney}
-  features_str = pd.DataFrame(data_user, index=[0])
+  data = {'BMI': BMI, 'Smoking': smoke, 'AlcoholDrinking': alc, 'Stroke': stroke, 'PhysicalHealth': physical, 'MentalHealth': mental, 'DiffWalking': climb, 'Sex': sex, 'AgeCategory': age_cat, 'Race': 'Asian', 'Diabetic': diabetes, 'PhysicalActivity': exercise, 'GenHealth': gen_health,'SleepTime': sleep, 'Asthma': asthma, 'KidneyDisease': kidney, 'SkinCancer': cancer}
+  features = pd.DataFrame(data, index=[0])
   st.subheader('Given Inputs : ')
-  st.write(features_str)
+  st.write(features)
   
-  ##Race Factor for Asians
-  bin_race = 3
+  return features
 
-  features_user = np.array([[bin_race, BMI, bin_smoke, bin_alc, bin_stroke, physical, mental, bin_sex, bin_diabetes, bin_exercise, sleep, factor_age, bin_asthma, bin_kidney]])
-  features_user_2 = np.array([[BMI, bin_smoke, bin_alc, bin_stroke, physical, mental, bin_sex, bin_diabetes, bin_exercise, sleep, factor_age, bin_asthma, bin_kidney]])
-  return features_user, features_user_2
+user = user_input_features()
+
+##Transform data
+
+discrete = ['HeartDisease', 'Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'AgeCategory',
+       'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth','Asthma', 'KidneyDisease', 'SkinCancer']     
+
+discrete2 = ['Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'Sex', 'AgeCategory',
+       'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth','Asthma', 'KidneyDisease', 'SkinCancer']     
+
+from sklearn.preprocessing import OrdinalEncoder
+enc = OrdinalEncoder()
+enc.fit(newdf[discrete])
+newdf[discrete] = enc.transform(newdf[discrete])
+
+enc.fit(user[discrete2])
+user[discrete2] = enc.transform(user[discrete2])
+
+##Testing
+##st.write(user)
+##st.write(newdf)
 
 
 
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-data.Smoking.replace(('Yes','No'), (1,0), inplace = True)
-data.HeartDisease.replace(('Yes','No'), (1,0), inplace = True)
-data.AlcoholDrinking.replace(('Yes','No'), (1,0), inplace = True)
-data.Stroke.replace(('Yes', "No"), (1,0), inplace = True)
-data.Sex.replace(('Male','Female'), (1,0), inplace = True)
-data.Asthma.replace(('Yes','No'), (1,0), inplace = True)
-data.Diabetic.replace(('Yes','No','No, borderline diabetes','Yes (during pregnancy)'), (1,0,0,0), inplace = True)
-data.PhysicalActivity.replace(('Yes','No'), (1,0), inplace = True)
-data.KidneyDisease.replace(('Yes','No'), (1,0), inplace = True)
-data.AgeCategory.replace(('18-24','25-29','30-34','35-39','40-44','45-49','50-54',
-                          '55-59','60-64','65-69','70-74','75-79','80 or older'),
-                         (0,0,1,1,1,2,2,2,3,3,4,5,6), inplace = True)
-data.Race.replace(('White', 'Black', 'Asian', 'American Indian/Alaskan Native', 'Other', 'Hispanic'),
-                          (1,2,3,1,1,2), inplace = True)
+# Splitting data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(newdf, y, test_size=0.1, random_state=12)
 
-column = ['Race','BMI','Smoking','AlcoholDrinking','Stroke','PhysicalHealth','MentalHealth','Sex','Diabetic','PhysicalActivity','SleepTime','AgeCategory','Asthma','KidneyDisease']
+# Creating feature matrix (param) and target vector (target)
+param = newdf.iloc[:, 0:17].values
+target = newdf.iloc[:, [0]].values
 
-# Split the data into training and testing sets
-X = data[column]
-Y = data.HeartDisease
+# Training ExtraTreesClassifier
+model = ExtraTreesClassifier()
+model.fit(param, target)
 
-# Use SMOTE to oversample the minority class
-from imblearn.over_sampling import SMOTE
-sm = SMOTE(random_state = 0)
-X_resampled, Y_resampled = sm.fit_resample(X, Y)
+# Making predictions using ExtraTreesClassifier
+prediction = model.predict(user)
+st.subheader('Prediction using ExtraTreesClassifier:')
+df1 = pd.DataFrame(prediction, columns=['0'])
+df1.loc[df1['0'] == 0, 'Chances of Heart Disease'] = 'No'
+df1.loc[df1['0'] == 1, 'Chances of Heart Disease'] = 'Yes'
+st.write(df1)
 
-# Split the resampled data into training and testing sets
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(X_resampled, Y_resampled, test_size = 0.2, random_state = 0)
+prediction_proba = model.predict_proba(user)
+st.subheader('Prediction Probability in % :')
+st.write(prediction_proba * 100)
 
-# Train a Decision Tree classifier on the training data
-from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier()
-model.fit(X_train, Y_train)
+# Training RandomForestClassifier
+model = RandomForestClassifier()
+model.fit(param, target)
 
-# Use the model to make predictions on the user's input
-result = user_input_features()
-features_users = result[0]
-prediction = model.predict(features_users)
-prediction_proba = model.predict_proba(features_users)
+# Making predictions using RandomForestClassifier
+prediction = model.predict(user)
+st.subheader('Prediction using RandomForestClassifier:')
+df1 = pd.DataFrame(prediction, columns=['0'])
+df1.loc[df1['0'] == 0, 'Chances of Heart Disease'] = 'No'
+df1.loc[df1['0'] == 1, 'Chances of Heart Disease'] = 'Yes'
+st.write(df1)
 
-# Display the prediction and probability to the user
-st.subheader("Prediction:")
-
-# Create a dataframe with the prediction
-prediction_df = pd.DataFrame(prediction, columns=["Prediction"])
-
-# Add a column with the chances of heart disease
-prediction_df["Result"] = prediction_df["Prediction"].map({0: "You are not likely to have heart disease", 1: "You might be at risk for heart disease, further steps are listed below:"})
-
-# Drop the original prediction column
-prediction_df = prediction_df.drop("Prediction", axis=1)
-st.dataframe(prediction_df)
-
-column2 = ['BMI','Smoking','AlcoholDrinking','Stroke','PhysicalHealth','MentalHealth','Sex','Diabetic','PhysicalActivity','SleepTime','AgeCategory','Asthma','KidneyDisease']
-
-# Select the subset of columns
-subset = df[column2]
-
-# Calculate the means and standard deviations of the subset of columns
-means = subset.mean()
-stds = subset.std()
-
-# Create a dataframe with the means and standard deviations
-stats_df = pd.concat([means, stds], axis=1)
-stats_df.columns = ['Mean', 'Standard Deviation']
-
-# Display the means and standard deviations
-st.header("Further Steps")
-st.write("Although our model does not produce answers at 100\% accuracy, we do suggest reaching out to medical professionals if at risk for heart disease.")
-st.write("Here is a list of resources to learn more about heart disease. The first link provided offers instructions in multiple languages. Further information is provided on the Health Recommendation Pages")
-st.markdown("[National Heart, Lung, and Blood Institute (NHLBI): Heart Disease](https://www.nhlbi.nih.gov/health-topics/heart-disease)")
-st.markdown("[Health Resources and Services Administration (HRSA): Heart Disease](https://www.hrsa.gov/health-topics/heart-disease)")
-st.markdown("[Centers for Disease Control and Prevention (CDC): Heart Disease Prevention](https://www.cdc.gov/heartdisease/prevention.htm)")
-st.text("\n")
-
-st.write("We have provided the means for each category below, we recommend you compare scores to learn which areas you can improve on. More information is provided on the Health Recommendation Page")
-st.dataframe(stats_df)
+prediction_proba = model.predict_proba(user)
+st.subheader('Prediction Probability in % :')
+st.write(prediction_proba * 100)
